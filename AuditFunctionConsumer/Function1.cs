fa7ce21b1555c98b1fbfff5f68dfed4b8be2f77e
karthik diwakar <karthikdiwakar123@gmail.com>
2026-01-14 13:40:52 -0500

    Add project files.

---
+++ AuditFunctionConsumer/Function1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AuditFunctionConsumer;
+
+public class AuditFunctionConsumer
+{
+    private readonly ILogger<AuditFunctionConsumer> _logger;
+
+    public AuditFunctionConsumer(ILogger<AuditFunctionConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(AuditFunctionConsumer))]
+    public async Task Run(
+        [ServiceBusTrigger("%ServiceBusConnection__queueName%", Connection = "ServiceBusConnection__fullyQualifiedNamespace")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Message ID: {id}", message.MessageId);
+        _logger.LogInformation("Message Body: {body}", message.Body);
+        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+        // Complete the message
+        await messageActions.CompleteMessageAsync(message);
+    }
+}
